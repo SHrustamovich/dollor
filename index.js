70852beaeda0248c751526df6699532ddd516e66
SHrustamovich <shohruhrustamovich5666@gmail.com>
2024-04-24 17:21:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// let a = "salom"
+
+// let b = "Amirbek"
+
+// // DOM
+
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+const title = document.querySelector(".title");
+button.addEventListener("click", () => {
+    title.textContent = `${(Number(input.value) * 12700).toLocaleString()} so'm`;
+});
+
+//
+
+// let a = 5
+// let b = 18
+// let c = 16
+
+// console.log(a === b || b === c || c === a)
+
+// let t = 90
+// let y = 78
+
+// t === y ? console.log("teng") : console.log("yo'q")
+
+let str2 = "frontend";
+let str3 = 90;
+
+// console.log(str2)
+// console.log(str3)
+
+// console.log(str2 + str3)
+
+// console.log(str2 - str3)
+
+// console.log(Math.floor(Math.random() * 100));
+
+// console.log(input.value);
+
+
+// 100 - 999   123 - bir yuz yigirma uch
+// 125 - bir yuz yigirma besh
+
+// 0-nol
+// 1-bir
+// 2-ikki
+// ...
+
